8c92f5590c3c4b9bd0778936ae3d5de2cdab947d
Chris Butler <chris.butler@redhat.com>
2025-06-18 18:40:57 +0900

    Initial commit

---
+++ install-gittuf.sh
@@ -0,0 +1,21 @@
+ARCH=amd64
+# One of: linux, darwin, freebsd
+OS=darwin
+# See https://github.com/gittuf/gittuf/releases for the latest version
+VERSION=0.11.0
+cd $(mktemp -d)
+
+curl -LO https://github.com/gittuf/gittuf/releases/download/v${VERSION}/gittuf_${VERSION}_${OS}_${ARCH}
+curl -LO https://github.com/gittuf/gittuf/releases/download/v${VERSION}/gittuf_${VERSION}_${OS}_${ARCH}.sig
+curl -LO https://github.com/gittuf/gittuf/releases/download/v${VERSION}/gittuf_${VERSION}_${OS}_${ARCH}.pem
+
+cosign verify-blob \
+    --certificate gittuf_${VERSION}_${OS}_${ARCH}.pem \
+    --signature gittuf_${VERSION}_${OS}_${ARCH}.sig \
+    --certificate-identity https://github.com/gittuf/gittuf/.github/workflows/release.yml@refs/tags/v${VERSION} \
+    --certificate-oidc-issuer https://token.actions.githubusercontent.com \
+    gittuf_${VERSION}_${OS}_${ARCH}
+
+sudo install gittuf_${VERSION}_${OS}_${ARCH} /usr/local/bin/gittuf
+cd -
+gittuf version
